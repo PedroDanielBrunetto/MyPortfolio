e491dad44db5817f47f3fd427fe28dad1e834a11
PedroDanielBrunetto <pedrodanielbm@hotmail.com>
2023-06-03 16:40:02 -0300

    navbar-sucessfull

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -4,8 +4,15 @@ import './Navbar.css';
 import Menu from '../../public/menu.svg';
 import { TypeAnimation } from 'react-type-animation';
 import Image from 'next/image';
+import { useState, useRef } from 'react';// Gerenciar o estado de algum valor
 
 export default function Navbar() {
+    const dropDownRef = useRef(null);
+    const [isActive, setIsActive] = useState(false);
+    const onClick = () => setIsActive(!isActive);
+
+    console.log(isActive)
+
     return (
         <header className="w-full">
             <div className="logo">
@@ -25,8 +32,10 @@ export default function Navbar() {
                     />
                </h1>
             </div>
-            <Image id="menu-icon" className='w-10' src={Menu} alt="Menu Icon" />
-            <ul className="navbar flex">
+            <button onClick={onClick}>
+                <Image className='menu-icon w-10' src={Menu} alt="Menu Icon" />
+            </button>
+            <ul ref={dropDownRef} className={`flex ${isActive ? "active" : "navbar"}`}>
                 <li><a href="#inicio">Inicio</a></li>
                 <li><a href="#sobre">Sobre mim</a></li>
                 <li><a href="#habilidades">Habilidades</a></li>
