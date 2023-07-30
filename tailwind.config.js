10216321a27cba965fc422c084a08a6a39583b0d
PedroDanielBrunetto <pedrodanielbm@hotmail.com>
2023-07-30 15:46:51 -0300

    fix-tailwindconfig

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ module.exports = {
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/*/.jsx',
+    './src/*/.js',
+    '.src/components/CardProjects/CardProjects.jsx'
   ],
   theme: {
     extend: {
