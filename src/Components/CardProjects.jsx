3da2db1d7c1cde50c0e03f69481f25fc20890117
PedroDanielBrunetto <pedrodanielbm@hotmail.com>
2023-07-30 14:40:57 -0300

    feat-testtailwind

--- src/Components/CardProjects.jsx
+++ src/Components/CardProjects.jsx
@@ -3,7 +3,7 @@ import { GitBranch } from 'phosphor-react'
 
 export default function CardProjects(props){
   return(
-    <div className="bg-[#1e3932] rounded-lg w-4/12 md:w-full items-center flex flex-col p-4 gap-3 h-[600px] md:flex md:flex-col md:h-auto">
+    <div className="flex flex-col bg-[#1e3932] rounded-lg w-4/12  items-center p-4 gap-3 h-[600px] md:flex md:flex-col md:h-auto md:w-full">
       <Image src={props.imagem} alt="Project" className={`-mt-36 ${props.size}`}/>
       <div className={`flex flex-col gap-5 -mt-10 ${props.estilo}`}>
         <h1 className="text-2xl text-[#ebdbc8] text-center">{props.nome}</h1>
@@ -19,7 +19,7 @@ export default function CardProjects(props){
           <Image src={props.tecnologias3} alt="Tecnologies" className="w-6 md:w-6 md:h-6"/>
           <Image src={props.tecnologias4} alt="Tecnologies" className="w-6 md:w-6 md:h-6 "/>
         </div>
-        <a href={props.url} className="text-base text-[#ebdbc8] flex justify-center items-center gap-4">
+        <a href={props.url} className="flex justify-center items-center text-base text-[#ebdbc8] gap-4">
         Veja mais: <GitBranch size={32} color="#00adc2"/>
         </a>
       </div>
