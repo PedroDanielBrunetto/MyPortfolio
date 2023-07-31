b444cdec5906d98f2ef2ef976ace91c13d7016f5
PedroDanielBrunetto <pedrodanielbm@hotmail.com>
2023-07-31 00:00:31 -0300

    aaaaa

--- src/Components/CardProjects/CardProjects.jsx
+++ src/Components/CardProjects/CardProjects.jsx
@@ -4,7 +4,7 @@ import { GitBranch } from 'phosphor-react'
 export default function CardProjects(props){
   return(
     <div className="flex flex-col bg-[#1e3932] rounded-lg w-4/12 items-center p-4 gap-3 min-h-[600px] md:min-h-[550px] md:flex md:flex-col md:h-auto md:w-full">
-      <div className="-mt-40 md:-mt-32">
+      <div className={`-mt-40 md:-mt-32 ${props.martop}`}>
         <Image src={props.imagem} alt="Project" />
       </div>
       <div className="flex flex-col gap-5">
