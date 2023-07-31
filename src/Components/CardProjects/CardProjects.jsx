11b126c9bd95812f9d485012f61775bea2fe523c
PedroDanielBrunetto <pedrodanielbm@hotmail.com>
2023-07-30 23:29:48 -0300

    testing-between

--- src/Components/CardProjects/CardProjects.jsx
+++ src/Components/CardProjects/CardProjects.jsx
@@ -3,15 +3,19 @@ import { GitBranch } from 'phosphor-react'
 
 export default function CardProjects(props){
   return(
-    <div className="flex flex-col bg-[#1e3932] rounded-lg w-4/12 items-center p-4 gap-3 max-h-[600px] md:flex md:flex-col md:h-auto md:w-full">
-      <div className="-mt-36">
+    <div className="flex flex-col justify-between bg-[#1e3932] rounded-lg w-4/12 items-center p-4 max-h-[600px] md:flex md:flex-col md:h-auto md:w-full">
+      <div className="-mt-10">
         <Image src={props.imagem} alt="Project" />
       </div>
-      <div className="flex flex-col gap-5">
-        <h1 className="text-2xl text-[#ebdbc8] text-center">{props.nome}</h1>
-        <p className="text-center text-lg text-[#ebdbc8] md:text-base">
-          {props.about}
-        </p>
+      <div className="flex flex-col">
+        <div className="flex">
+          <h1 className="text-2xl text-[#ebdbc8] text-center">{props.nome}</h1>
+        </div>
+        <div className="flex">
+          <p className="text-center text-lg text-[#ebdbc8] md:text-base">
+            {props.about}
+          </p>
+        </div>
         <div className="flex justify-center gap-2 md:gap-1">
           <p className="text-base text-[#ebdbc8]">
             Tecnologias usadas:
@@ -29,9 +33,11 @@ export default function CardProjects(props){
             <Image src={props.tecnologias4} alt="Tecnologies"/>
           </div>
         </div>
-        <a href={props.url} className="flex justify-center items-center text-base text-[#ebdbc8] gap-4">
-          Veja mais: <GitBranch size={32} color="#00adc2"/>
-        </a>
+        <div className="flex">
+          <a href={props.url} className="flex justify-center items-center text-base text-[#ebdbc8] gap-4">
+            Veja mais: <GitBranch size={32} color="#00adc2"/>
+          </a>
+        </div>
       </div>
     </div>
   )
